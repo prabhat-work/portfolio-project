7449a773645ad8094e96f6910c3975a499bda467
stebin <stebinbensb@gmail.com>
2018-12-06 08:28:06 +0530

    added boostrap front page dev and jobs with ui

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class blog(models.Model):
+    title=models.CharField(max_length=10)
+    pub_date=models.DateTimeField()
+    body=models.CharField(max_length=100)
+    image=models.ImageField(upload_to='images/')
