62afeb770fdbf4f11d9a5828af35fc591d8f645d
stebin <stebinbensb@gmail.com>
2018-12-08 00:30:56 +0530

    Blog and Jobs polished well

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 
 class blog(models.Model):
-    title=models.CharField(max_length=10)
+    title=models.TextField(max_length=1000)
     pub_date=models.DateTimeField()
-    body=models.CharField(max_length=100)
+    body=models.CharField(max_length=1000)
     image=models.ImageField(upload_to='images/')
+
+def __str__(self):
+    return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 
-# Create your views here.
+from .models import blog
+
+def allblogs(request):
+    blogs=blog.objects
+    return render(request,'blog/home.html',{'blogs':blogs})
+
+def detail(request,blog_id):
+    details=get_object_or_404(blog , pk=blog_id)
+    return render(request,'blog/detail.html',{'details':details})
+
+
+
+
+
